bcc872d322566bb6d1887e65da9fb5285524791e
X <alternx7@yahoo.com>
2021-07-30 21:05:28 -0500

    Google SignIn added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify.js')
+
 
 const login = async(req, res = response) => {
 
@@ -49,11 +51,56 @@ const login = async(req, res = response) => {
         });
     }
 
+}
+
+const googleSignin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
 
 
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                //Este valor no importa sólo se pone de relleno porque se autentica con google
+                password: ':P',
+                img,
+                google: true
+
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        //Generar jwt
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+    }
 
 }
 
 module.exports = {
-    login
+    login,
+    googleSignin
 }
